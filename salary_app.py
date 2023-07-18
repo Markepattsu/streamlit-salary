ec20c76fa951a677c3c27e046beec76db212cd0f
Markepattsu <137873999+Markepattsu@users.noreply.github.com>
2023-07-19 02:13:01 +0800

    salary_app.py

--- salary_app.py
+++ salary_app.py
@@ -21,8 +21,6 @@ st.sidebar.header('User Input Features')
 
 def user_input_features():
     
-    data = {}
-
     age_ranges = ['Under 18', '18-34', '35-54', '55 or over']
     selected_age_range = st.sidebar.selectbox('Age Range', age_ranges)
     
@@ -54,8 +52,8 @@ def user_input_features():
         'Transport or Logistics',
         'Utilities & Telecommunications',
     ]       
-    
     selected_industries = st.sidebar.selectbox('Industries', industries)
+    
     continents = [
         'Africa',
         'Asia',
@@ -109,6 +107,14 @@ def user_input_features():
     ]
     selected_gender = st.sidebar.radio('Gender', genders)
 
+    data = {'New_Age_Range': age_ranges,
+           'What_industry_do_you_work_in': industries,
+           'Continent': continents,
+           'How_many_years_of_professional_work_experience_do_you_have_overall': experience_overall,
+           'How_many_years_of_professional_work_experience_do_you_have_in_your_field': experience_field,
+           'What_is_your_highest_level_of_education_completed': degrees,
+           'What_is_your_gender': genders}
+
     # data['Age Range'] = selected_age_range
     # data['Industries'] = selected_industries
     # data['Continents'] = selected_continents
@@ -118,26 +124,26 @@ def user_input_features():
     # data['Gender'] = selected_gender
 
                                                      
-    for age_range in age_ranges:
-        data[f'New_Age_Range_{age_range}'] = age_range == selected_age_range
+    # for age_range in age_ranges:
+    #     data[f'New_Age_Range_{age_range}'] = age_range == selected_age_range
 
-    for industry in industries:
-        data[f'What_industry_do_you_work_in__{industry}'] = industry in selected_industries
+    # for industry in industries:
+    #     data[f'What_industry_do_you_work_in__{industry}'] = industry in selected_industries
 
-    for continent in continents:
-        data[f'Continent_{continent}'] = continent in selected_continents
+    # for continent in continents:
+    #     data[f'Continent_{continent}'] = continent in selected_continents
 
-    for exp in experience_overall:
-        data[f'How_many_years_of_professional_work_experience_do_you_have_overall__{exp}'] = exp == selected_experience_overall
+    # for exp in experience_overall:
+    #     data[f'How_many_years_of_professional_work_experience_do_you_have_overall__{exp}'] = exp == selected_experience_overall
     
-    for exp in experience_field:
-        data[f'How_many_years_of_professional_work_experience_do_you_have_in_your_field__{exp}'] = exp == selected_experience_field
+    # for exp in experience_field:
+    #     data[f'How_many_years_of_professional_work_experience_do_you_have_in_your_field__{exp}'] = exp == selected_experience_field
 
-    for degree in degrees:
-        data[f'What_is_your_highest_level_of_education_completed__{degree}'] = degree in selected_degree
+    # for degree in degrees:
+    #     data[f'What_is_your_highest_level_of_education_completed__{degree}'] = degree in selected_degree
 
-    for gender in genders:
-        data[f'What_is_your_gender__{gender}'] = gender in selected_gender
+    # for gender in genders:
+    #     data[f'What_is_your_gender__{gender}'] = gender in selected_gender
 
 
     features = pd.DataFrame(data, index=[0])
