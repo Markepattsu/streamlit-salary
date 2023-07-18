5cf47372ed564381b1ccd1137b569aa1b870a5bf
Markepattsu <137873999+Markepattsu@users.noreply.github.com>
2023-07-18 22:29:30 +0800

    salary_app.py

--- salary_app.py
+++ salary_app.py
@@ -176,7 +176,7 @@ else:
 
 
 
-model = joblib.load(r'C:\Users\tanak\OneDrive\Desktop\Desk\Work\Yr 2\Sem 1\MLDP\Proj\model_lgbm.pkl')
+model = joblib.load('C:/Users/tanak/OneDrive/Desktop/Desk/Work/Yr 2/Sem 1/MLDP/Proj/model_lgbm.pkl')
 
 
 if os.path.exists(model_path):
