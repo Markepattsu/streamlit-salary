4e4805d872eb6bc52232502ccda4b8946e3bb19d
Markepattsu <137873999+Markepattsu@users.noreply.github.com>
2023-07-19 20:55:38 +0800

    salary_app.py

--- salary_app.py
+++ salary_app.py
@@ -11,7 +11,7 @@ import sklearn.externals
 import os
 
 
-st.image('https://free4kwallpapers.com/uploads/originals/2015/07/15/working-hard.jpg', width=1920)
+st.image('https://free4kwallpapers.com/uploads/originals/2015/07/15/working-hard.jpg', width=1080)
 
 
 st.write("""
